72d6cf7cd7c1063c05dc831c01ae64a1ad61e5fb
Machiel de Jager <dejager.mc@gmail.com>
2019-01-22 15:15:19 +0100

    Java 11 fix

--- src/main/java/nl/dejagermc/homefeeder/HomefeederApplication.java
+++ src/main/java/nl/dejagermc/homefeeder/HomefeederApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class HomefeederApplication {
 
     public static void main(String[] args) {
