7a59ca900bcb11fc88e7c0cf0be42e93aca1d4e3
Machiel de Jager <dejager.mc@gmail.com>
2019-03-07 07:50:23 +0100

    Feature: tradfri, klein vervolg op maken coap client

--- src/main/java/nl/dejagermc/homefeeder/output/tradfri/TradfriOutput.java
+++ src/main/java/nl/dejagermc/homefeeder/output/tradfri/TradfriOutput.java
@@ -5,11 +5,19 @@ import org.eclipse.californium.core.CoapClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import org.eclipse.californium.scandium.config.DtlsConnectorConfig;
+
+import java.net.InetSocketAddress;
+
 @Service
 @Slf4j
 public class TradfriOutput {
+    private static final String COAP = "coaps://";
+    private static final String REBOOT = "/15011/9030";
+
     @Value("${tradfri.ip}")
     private String ip;
+    private String port = ":5684";
 
     private final String userName = "homefeeder";
 
@@ -24,5 +32,10 @@ public class TradfriOutput {
 
     private void setSharedKey(CoapClient client) {
         client.post("", 0);
+    }
+
+    public void test() {
+        DtlsConnectorConfig.Builder builder = new DtlsConnectorConfig.Builder();
+//        builder.setPskStore()
     }
 }
